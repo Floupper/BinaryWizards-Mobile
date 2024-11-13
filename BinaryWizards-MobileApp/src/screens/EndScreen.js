20837c454e0e15ff7fcbbc7abf34f96cfc674f10
Eliott Schott <eliott.schott.eliott@gmail.com>
2024-11-13 13:38:29 +0100

    feat: add restart quiz button

--- BinaryWizards-MobileApp/src/screens/EndScreen.js
+++ BinaryWizards-MobileApp/src/screens/EndScreen.js
@@ -1,8 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import { useNavigation } from "@react-navigation/native";
 import { styleContainer } from "../styles/container";
 import { styleText } from "../styles/text";
+import { resetQuiz } from "../services/requests";
 
 export default function EndScreen({ route }) {
   const { score, quizId } = route.params;
@@ -10,6 +11,10 @@ export default function EndScreen({ route }) {
 
   const backToHome = () => {
     navigation.navigate("Home");
+  };
+
+  const playAgain = () => {
+    resetQuiz(quizId, navigation);
   };
 
   return (
@@ -25,6 +30,11 @@ export default function EndScreen({ route }) {
           disabled={false}
           text={"Back to Home"}
           onPress={backToHome}
+        />
+        <PrimaryButton
+          disabled={false}
+          text={"Play Again"}
+          onPress={playAgain}
         />
       </View>
     </View>
