b4c17853cc6d7c7651aaab806d9a6ea1aba3a2cb
Eliott Schott <eliott.schott.eliott@gmail.com>
2024-11-22 14:55:16 +0100

    feature/4-login-screen

--- BinaryWizards-MobileApp/src/utils/axiosInstance.js
+++ BinaryWizards-MobileApp/src/utils/axiosInstance.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Toast from 'react-native-toast-message';
+import { REACT_NATIVE_API_URL, REACT_NATIVE_API_PORT } from '@env';
 
 const axiosInstance = axios.create({
-    baseURL: `${process.env.REACT_NATIVE_API_URL}:${process.env.REACT_NATIVE_API_PORT}`,
+    baseURL: `${REACT_NATIVE_API_URL}:${REACT_NATIVE_API_PORT}`,
     headers: {
         'Content-Type': 'application/json',
     },
