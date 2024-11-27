310e2d178ecd8d6531b51007907e77d47ffcd9c7
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2024-11-27 14:47:40 +0100

    feat: add comments in navigation files

--- BinaryWizards-MobileApp/src/navigation/DashboardTab.js
+++ BinaryWizards-MobileApp/src/navigation/DashboardTab.js
@@ -9,27 +9,41 @@ const Tab = createBottomTabNavigator();
 export default function DashboardTab() {
   return (
     <Tab.Navigator
+
+      // Configure screen options dynamically based on the route
       screenOptions={({ route }) => ({
+
+        // Define the icon for each tab
         tabBarIcon: ({ color, size }) => {
           let iconName;
 
+          // Assign icon names based on the route name
           if (route.name === "Dashboard") {
-            iconName = "home";
+            iconName = "home"; // Icon for the "Dashboard" tab
           } else if (route.name === "Statistics") {
-            iconName = "linechart";
+            iconName = "linechart"; // Icon for the "Statistics" tab
           }
 
+          // Return the icon component with the specified properties
           return <Icon name={iconName} size={size} color={color} />;
         },
+
+        // Set the color for the active tab icon
         tabBarActiveTintColor: "#3552b0",
+
+        // Set the color for the inactive tab icon
         tabBarInactiveTintColor: "gray",
       })}
     >
+
+      {/* Define the "Dashboard" tab */}
       <Tab.Screen
         name="Dashboard"
         component={Dashboard}
         options={{ headerShown: false }}
       />
+
+      {/* Define the "Statistics" tab */}
       <Tab.Screen
         name="Statistics"
         component={Statistics}
