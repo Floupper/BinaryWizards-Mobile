75acd08e4fd1ccc95f0fef5693dce731856cc423
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2024-11-27 15:04:19 +0100

    feat: add comments in utils files

--- BinaryWizards-MobileApp/src/utils/asyncStorage.js
+++ BinaryWizards-MobileApp/src/utils/asyncStorage.js
@@ -1,56 +1,77 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Toast from "react-native-toast-message";
 
+// Retrieve the user token from async storage
 export async function _retrieveUserToken(navigation) {
   try {
+
     const value = await AsyncStorage.getItem("userToken");
-    if(value === null) {
+
+    if (value === null) {
       logout(navigation);
     }
+
     return value;
   } catch (error) {
+
     Toast.show({
       type: "error",
       text1: "Error",
       text2: "An error occured while retrieving user token",
     });
+
     console.error("Error retrieving user token:", error);
+
     navigation.navigate("Home");
   }
 }
 
+// Remove the user token from async storage
 export async function _removeUserToken() {
   try {
+
     await AsyncStorage.removeItem("userToken");
+
   } catch (error) {
+
     Toast.show({
       type: "error",
       text1: "Error",
       text2: "An error occured while logging out",
     });
+
     console.error("Error removing user token:", error);
   }
 }
 
 export async function _storeUserToken(token) {
   try {
+
     await AsyncStorage.setItem("userToken", token);
+
   } catch (error) {
+
     Toast.show({
       type: "error",
       text1: "Error",
       text2: "An error occured while storing user token",
     });
+
     console.error("Error storing user token:", error);
   }
 }
 
+// Logout the user by removing the user token from async storage
 export const logout = async (navigation) => {
   try {
+
     await _removeUserToken();
     navigation.navigate("Home");
+
   } catch (error) {
+
     console.error("Error logging out:", error);
+
     Toast.show({
       type: "error",
       text1: "Error logging out",
