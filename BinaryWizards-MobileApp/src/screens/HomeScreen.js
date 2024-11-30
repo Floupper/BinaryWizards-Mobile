e7c0a4049d379385f88890b7733023e917f6307b
Eliott Schott <eliott.schott.eliott@gmail.com>
2024-11-30 17:27:57 +0100

    feat: added creat quiz button on logged user screen

--- BinaryWizards-MobileApp/src/screens/HomeScreen.js
+++ BinaryWizards-MobileApp/src/screens/HomeScreen.js
@@ -6,11 +6,15 @@ import { styleContainer } from '../styles/container';
 import TopBar from '../components/TopBar';
 import UserHomeComponent from '../components/UserHomeComponent';
 import HomeComponent from '../components/HomeComponent';
+import PrimaryButton from '../components/PrimaryButton';
+import { styleButton } from '../styles/buttons';
+import { useNavigation } from '@react-navigation/native';
 
 const queryClient = new QueryClient();
 
 export default function HomeScreen() {
   const [userToken, setUserToken] = useState(null);
+  const navigation = useNavigation();
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -19,6 +23,12 @@ export default function HomeScreen() {
         {userToken ? (
           <View style={styleContainer.container}>
             <UserHomeComponent />
+            <View style={styleContainer.divider} />
+            <PrimaryButton
+              text="Create quiz"
+              onPress={() => navigation.navigate('Create')}
+              style={styleButton.button}
+            />
           </View>
         ) : (
           <View style={styleContainer.container}>
