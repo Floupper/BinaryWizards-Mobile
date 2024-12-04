f1baa5ff6283df73907e1f55e0f9a3f1d4ff3a35
Eliott Schott <eliott.schott.eliott@gmail.com>
2024-12-04 09:13:27 +0100

    fix: user is disconnected if token is invalid while fetching categories and difficulties

--- BinaryWizards-MobileApp/src/screens/CreateGame.js
+++ BinaryWizards-MobileApp/src/screens/CreateGame.js
@@ -1,153 +1,163 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { View, Text, StyleSheet, Button, TextInput } from "react-native";
-import Toast from "react-native-toast-message";
-import { SelectList } from "react-native-dropdown-select-list";
+import React, { useEffect, useState, useCallback } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import Toast from 'react-native-toast-message';
+import { SelectList } from 'react-native-dropdown-select-list';
 
-import { styleContainer } from "../styles/container";
-import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { styleContainer } from '../styles/container';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 
 import {
-    fetchAndCreateQuiz,
-    fetchCategories,
-    fetchDifficulties,
-} from "../services/createGame";
-import PrimaryButton from "../components/PrimaryButton";
-import { styleButton } from "../styles/buttons";
+  fetchAndCreateQuiz,
+  fetchCategories,
+  fetchDifficulties,
+} from '../services/createGame';
+import PrimaryButton from '../components/PrimaryButton';
+import { styleButton } from '../styles/buttons';
 
 export default function CreateGame() {
-    const [categories, setCategories] = useState([]);
-    const [selectedCategory, setSelectedCategory] = useState("");
-    const [nbQuestions, setNbQuestions] = useState("10");
-    const [difficulty, setDifficulty] = useState("");
-    const [difficulties, setDifficulties] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [nbQuestions, setNbQuestions] = useState('10');
+  const [difficulty, setDifficulty] = useState('');
+  const [difficulties, setDifficulties] = useState([]);
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    useEffect(() => {
-        (async () => {
-            setCategories(await fetchCategories());
-        })();
+  useEffect(() => {
+    (async () => {
+      let categories = await fetchCategories();
+      if (!categories) {
+        setCategories([]);
+        navigation.navigate('Home');
+      }
+      setCategories(categories);
+    })();
 
-        (async () => {
-            setDifficulties(await fetchDifficulties());
-        })();
-    }, []);
+    (async () => {
+      let difficulties = await fetchDifficulties();
+      if (!difficulties) {
+        setDifficulties([]);
+        navigation.navigate('Home');
+      }
+      setDifficulties(difficulties);
+    })();
+  }, []);
 
-    useFocusEffect(
-        useCallback(() => {
-            setNbQuestions("10");
-            setSelectedCategory("");
-            setDifficulty("");
-        }, [])
-    );
+  useFocusEffect(
+    useCallback(() => {
+      setNbQuestions('10');
+      setSelectedCategory('');
+      setDifficulty('');
+    }, [])
+  );
 
-    const handleNbQuestionsChange = (value) => {
-        const numericValue = parseInt(value, 10);
-        if (!isNaN(numericValue) && numericValue >= 1 && numericValue <= 50) {
-            setNbQuestions(value);
-        } else {
-            Toast.show({
-                type: "error",
-                text1: "Invalid Input",
-                text2: "Please enter a number between 1 and 50",
-                position: "bottom",
-            });
-            setNbQuestions(""); // Reset field if value is not valid
-        }
-    };
+  const handleNbQuestionsChange = (value) => {
+    const numericValue = parseInt(value, 10);
+    if (!isNaN(numericValue) && numericValue >= 1 && numericValue <= 50) {
+      setNbQuestions(value);
+    } else {
+      Toast.show({
+        type: 'error',
+        text1: 'Invalid Input',
+        text2: 'Please enter a number between 1 and 50',
+        position: 'bottom',
+      });
+      setNbQuestions(''); // Reset field if value is not valid
+    }
+  };
 
-    return (
-        <View style={styleContainer.container}>
-            <View style={styles.pickerContainer}>
-                <Text style={styles.label}>Category</Text>
-                <SelectList
-                    setSelected={(value) => setSelectedCategory(value)}
-                    data={categories}
-                    placeholder="Select a category"
-                    boxStyles={styles.input}
-                    dropdownStyles={styles.selectListDropdown}
-                />
-            </View>
+  return (
+    <View style={styleContainer.container}>
+      <View style={styles.pickerContainer}>
+        <Text style={styles.label}>Category</Text>
+        <SelectList
+          setSelected={(value) => setSelectedCategory(value)}
+          data={categories}
+          placeholder="Select a category"
+          boxStyles={styles.input}
+          dropdownStyles={styles.selectListDropdown}
+        />
+      </View>
 
-            <View style={styles.pickerContainer}>
-                <Text style={styles.label}>Number of question</Text>
-                <TextInput
-                    style={styles.input}
-                    keyboardType="numeric"
-                    placeholder="Enter the number of questions"
-                    value={nbQuestions}
-                    onChangeText={handleNbQuestionsChange}
-                    maxLength={2} // Limit input to 2 characters
-                />
-            </View>
+      <View style={styles.pickerContainer}>
+        <Text style={styles.label}>Number of question</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          placeholder="Enter the number of questions"
+          value={nbQuestions}
+          onChangeText={handleNbQuestionsChange}
+          maxLength={2} // Limit input to 2 characters
+        />
+      </View>
 
-            <View style={styles.pickerContainer}>
-                <Text style={styles.label}>Difficulty</Text>
-                <SelectList
-                    setSelected={(value) => setDifficulty(value)}
-                    data={difficulties}
-                    placeholder="Select a difficulty"
-                    boxStyles={styles.input}
-                    dropdownStyles={styles.selectListDropdown}
-                />
-            </View>
+      <View style={styles.pickerContainer}>
+        <Text style={styles.label}>Difficulty</Text>
+        <SelectList
+          setSelected={(value) => setDifficulty(value)}
+          data={difficulties}
+          placeholder="Select a difficulty"
+          boxStyles={styles.input}
+          dropdownStyles={styles.selectListDropdown}
+        />
+      </View>
 
-            <View>
-                <PrimaryButton
-                    text="Start"
-                    onPress={() =>
-                        fetchAndCreateQuiz(
-                            selectedCategory,
-                            nbQuestions,
-                            difficulty,
-                            navigation
-                        )
-                    }
-                    disabled={
-                        !nbQuestions ||
-                        isNaN(parseInt(nbQuestions, 10)) ||
-                        difficulty === ""
-                    }
-                    style={[
-                        styleButton.button,
-                        (!nbQuestions ||
-                            isNaN(parseInt(nbQuestions, 10)) ||
-                            difficulty === "") && { backgroundColor: "gray" },
-                    ]}
-                />
-            </View>
+      <View>
+        <PrimaryButton
+          text="Start"
+          onPress={() =>
+            fetchAndCreateQuiz(
+              selectedCategory,
+              nbQuestions,
+              difficulty,
+              navigation
+            )
+          }
+          disabled={
+            !nbQuestions ||
+            isNaN(parseInt(nbQuestions, 10)) ||
+            difficulty === ''
+          }
+          style={[
+            styleButton.button,
+            (!nbQuestions ||
+              isNaN(parseInt(nbQuestions, 10)) ||
+              difficulty === '') && { backgroundColor: 'gray' },
+          ]}
+        />
+      </View>
 
-            <Toast />
-        </View>
-    );
+      <Toast />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    pickerContainer: {
-        marginBottom: 20,
-        width: "100%",
-    },
-    label: {
-        fontSize: 16,
-        marginBottom: 5,
-        paddingHorizontal: 10,
-    },
-    input: {
-        height: 50,
-        width: "90%",
-        marginHorizontal: 12,
-        borderWidth: 1,
-        borderRadius: 10,
-        paddingHorizontal: 10,
-        fontSize: 14,
-        borderColor: "gray",
-        color: "black",
-    },
-    selectListDropdown: {
-        width: "90%",
-        marginHorizontal: 12,
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: "gray",
-    },
+  input: {
+    borderColor: 'gray',
+    borderRadius: 10,
+    borderWidth: 1,
+    color: 'black',
+    fontSize: 14,
+    height: 50,
+    marginHorizontal: 12,
+    paddingHorizontal: 10,
+    width: '90%',
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+    paddingHorizontal: 10,
+  },
+  pickerContainer: {
+    marginBottom: 20,
+    width: '100%',
+  },
+  selectListDropdown: {
+    borderColor: 'gray',
+    borderRadius: 10,
+    borderWidth: 1,
+    marginHorizontal: 12,
+    width: '90%',
+  },
 });
--- BinaryWizards-MobileApp/src/services/createGame.js
+++ BinaryWizards-MobileApp/src/services/createGame.js
@@ -1,171 +1,199 @@
-import { REACT_NATIVE_API_URL, REACT_NATIVE_API_PORT } from "@env";
-import Toast from "react-native-toast-message";
-import axiosInstance from "../utils/axiosInstance";
+import Toast from 'react-native-toast-message';
+import axiosInstance from '../utils/axiosInstance';
+import { _removeUserToken } from '../utils/asyncStorage';
 
 export const fetchCategories = async () => {
-    try {
-        const response = await axiosInstance.get('/categories/');
-        const data = response.data;
+  try {
+    const response = await axiosInstance.get('/categories/');
+    const data = response.data;
 
-        const formattedCategories = data.map((category) => ({
-            key: category.id,
-            label: category.name,
-            value: category.name,
-        }));
-
-        return formattedCategories;
-    } catch (error) {
-        Toast.show({
-            type: 'error',
-            text1: 'Error',
-            text2: 'An error occured while fetching the categories',
-        });
-        console.error("Error fetching categories", error);
+    if (response.status === 401) {
+      await _removeUserToken();
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2: 'Please login again',
+      });
+      return;
     }
+
+    const formattedCategories = data.map((category) => ({
+      key: category.id,
+      label: category.name,
+      value: category.name,
+    }));
+
+    return formattedCategories;
+  } catch (error) {
+    Toast.show({
+      type: 'error',
+      text1: 'Error',
+      text2: 'An error occured while fetching the categories',
+    });
+    console.error('Error fetching categories', error);
+  }
 };
 
 export const fetchDifficulties = async () => {
-    try {
-        const response = await axiosInstance.get("/difficulties");
-        const data = response.data;
+  try {
+    const response = await axiosInstance.get('/difficulties');
 
-        const formattedDifficulties = data.map((difficulty) => ({
-            label: difficulty,
-            value: String(difficulty).charAt(0).toUpperCase() + String(difficulty).slice(1),
-        }));
-
-        return formattedDifficulties;
-    } catch (error) {
-        Toast.show({
-            type: 'error',
-            text1: 'Error',
-            text2: 'An error occured while fetching the difficulties',
-        });
-        console.error("Error fetching difficulties", error);
+    if (response.status === 401) {
+      await _removeUserToken();
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2: 'Please login again',
+      });
+      return;
     }
+
+    const data = response.data;
+
+    const formattedDifficulties = data.map((difficulty) => ({
+      label: difficulty,
+      value:
+        String(difficulty).charAt(0).toUpperCase() +
+        String(difficulty).slice(1),
+    }));
+
+    return formattedDifficulties;
+  } catch (error) {
+    Toast.show({
+      type: 'error',
+      text1: 'Error',
+      text2: 'An error occured while fetching the difficulties',
+    });
+    console.error('Error fetching difficulties', error);
+  }
 };
 
 export const fetchQuestion = async ({ quizId }) => {
-    try {
-        const result = await axiosInstance.get(`/quiz/${quizId}/question`);
-        if (!result.ok) {
-            throw new Error(`HTTP error! Status: ${result.status}`);
-        }
-        const json = result.data;
-        return json;
-    } catch (error) {
-        Toast.show({
-            type: 'error',
-            text1: 'Error',
-            text2: 'An error occured while fetching the question',
-        });
-        console.error("Error fetching question:", error);
-        return null;
+  try {
+    const result = await axiosInstance.get(`/quiz/${quizId}/question`);
+
+    if (result.status == 401) {
+      await _removeUserToken();
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2: 'Please login again',
+      });
+      return;
     }
+    const json = result.data;
+    return json;
+  } catch (error) {
+    Toast.show({
+      type: 'error',
+      text1: 'Error',
+      text2: 'An error occured while fetching the question',
+    });
+    console.error('Error fetching question:', error);
+    return null;
+  }
 };
 
 export const sendAnswer = async ({ quizId, question_index, option_index }) => {
-    try {
-        const response = await axiosInstance.post(`/quiz/${quizId}/question`, {
-            question_index: question_index,
-            option_index: option_index,
-        });
+  try {
+    const response = await axiosInstance.post(`/quiz/${quizId}/question`, {
+      question_index: question_index,
+      option_index: option_index,
+    });
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = response.data;
-        return data;
-    } catch (error) {
-        Toast.show({
-            type: 'error',
-            text1: 'Error',
-            text2: 'An error occured while sending your answer',
-        });
-        console.error("Error:", error);
-        return null;
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
+
+    const data = response.data;
+    return data;
+  } catch (error) {
+    Toast.show({
+      type: 'error',
+      text1: 'Error',
+      text2: 'An error occured while sending your answer',
+    });
+    console.error('Error:', error);
+    return null;
+  }
 };
 
 export async function fetchAndCreateQuiz(
-    category,
-    nbQuestions,
-    difficulty,
-    navigation
+  category,
+  nbQuestions,
+  difficulty,
+  navigation
 ) {
+  const quizData = {};
 
-    const quizData = {};
+  if (!nbQuestions || nbQuestions < 1 || nbQuestions > 50) {
+    Toast.show({
+      type: 'error',
+      text1: 'Number of Questions',
+      text2: 'You must select a number of questions between 1 and 50',
+    });
+    return;
+  } else {
+    quizData.amount = Number(nbQuestions);
+  }
 
-    if (!nbQuestions || nbQuestions < 1 || nbQuestions > 50) {
-        Toast.show({
-            type: 'error',
-            text1: 'Number of Questions',
-            text2: 'You must select a number of questions between 1 and 50',
-        });
-        return;
-    } else {
-        quizData.amount = Number(nbQuestions);
+  if (!category || category === '') {
+    Toast.show({
+      type: 'error',
+      text1: 'Category',
+      text2: 'You must select a category',
+    });
+    return;
+  } else {
+    quizData.category = Number(category);
+  }
+
+  if (difficulty) {
+    quizData.difficulty = String(difficulty).toLowerCase();
+  }
+
+  try {
+    const createQuiz = await axiosInstance.post('/quiz', quizData);
+
+    if (!createQuiz) {
+      throw new Error('No response returned');
     }
 
-    if (!category || category === "") {
-        Toast.show({
-            type: 'error',
-            text1: 'Category',
-            text2: 'You must select a category',
-        });
-        return;
-    } else {
-        quizData.category = Number(category);
+    const data = createQuiz.data;
+
+    if (!data) {
+      throw new Error('No data returned');
     }
 
-    if (difficulty) {
-        quizData.difficulty = String(difficulty).toLowerCase();
-    }
-
-    try {
-        const createQuiz = await axiosInstance.post("/quiz", quizData);
-
-        if (!createQuiz) {
-            throw new Error("No response returned");
-        }
-
-        const data = createQuiz.data;
-
-        if (!data) {
-            throw new Error("No data returned");
-        }
-
-        createGameId(data.quiz_id, navigation);
-    } catch (error) {
-        Toast.show({
-            type: 'error',
-            text1: 'Error',
-            text2: 'An error occured while creating the quiz',
-        });
-        console.error("Error creating quiz:", error);
-    };
+    createGameId(data.quiz_id, navigation);
+  } catch (error) {
+    Toast.show({
+      type: 'error',
+      text1: 'Error',
+      text2: 'An error occured while creating the quiz',
+    });
+    console.error('Error creating quiz:', error);
+  }
 }
 
 export async function createGameId(quizId, navigation) {
-    try {
-        const gameResponse = await axiosInstance.get(`/game/${quizId}/create`);
-        if (gameResponse && gameResponse.data.game_id) {
-            navigation.navigate('Questions', { gameId: gameResponse.data.game_id });
-        }
-        else {
-            Toast.show({
-                type: 'error',
-                text1: 'Error',
-                text2: 'Please enter a valid code.',
-            });
-        }
-    } catch (error) {
-        Toast.show({
-            type: 'error',
-            text1: 'Error',
-            text2: 'An error occured while creating the game',
-        });
-        console.error("Error creating game:", error);
+  try {
+    const gameResponse = await axiosInstance.get(`/game/${quizId}/create`);
+    if (gameResponse && gameResponse.data.game_id) {
+      navigation.navigate('Questions', { gameId: gameResponse.data.game_id });
+    } else {
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2: 'Please enter a valid code.',
+      });
     }
+  } catch (error) {
+    Toast.show({
+      type: 'error',
+      text1: 'Error',
+      text2: 'An error occured while creating the game',
+    });
+    console.error('Error creating game:', error);
+  }
 }
