ba179176be9744e26c17091185391b5d56e78910
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2024-12-19 09:50:50 +0100

    feat(ShareModal): add link & QrCode

--- BinaryWizards-MobileApp/src/components/ShareModal/ShareModal.js
+++ BinaryWizards-MobileApp/src/components/ShareModal/ShareModal.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Modal, Button } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 import { styles } from './ShareModelStyle';
+import { REACT_NATIVE_API_URL, REACT_NATIVE_API_PORT } from '@env';
 
-export default function ShareModal({ visible, toggleModal }) {
+export default function ShareModal({ visible, toggleModal, gameId }) {
   const [link, setLink] = useState('');
   const [qrCode, setQrCode] = useState('');
+
+  useEffect(() => {
+    setLink(
+      `${process.env.REACT_NATIVE_API_URL}:${REACT_NATIVE_API_PORT}/join/${gameId}`
+    );
+  }, [gameId]);
 
   return (
     <Modal
@@ -18,7 +26,9 @@ export default function ShareModal({ visible, toggleModal }) {
         <View style={styles.modalContent}>
           <Text style={styles.modalTitle}>Share Options</Text>
           <Text>Link: {link}</Text>
-          <Text>QrCode: {qrCode}</Text>
+          <View style={{ marginVertical: 15 }}>
+            <QRCode value={link} />
+          </View>
           <Button title="Close" onPress={toggleModal} color={'black'} />
         </View>
       </View>
