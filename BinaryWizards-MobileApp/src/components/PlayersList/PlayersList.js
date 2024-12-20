a68658fb939301d0eea0080f11df899b273a90df
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2024-12-20 10:00:17 +0100

    feat(PlayerList): add create game & join game with websocket & display live playersList

--- BinaryWizards-MobileApp/src/components/PlayersList/PlayersList.js
+++ BinaryWizards-MobileApp/src/components/PlayersList/PlayersList.js
@@ -1,55 +1,120 @@
 import React, { useState, useEffect } from 'react';
-import { Text } from 'react-native';
+import { Button, Text } from 'react-native';
 import { io } from 'socket.io-client';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { View } from 'react-native';
 
 import { styles } from './PlayersListStyle';
-import { REACT_NATIVE_SOCKET_SERVER_URL } from '@env';
+import { REACT_NATIVE_API_URL, REACT_NATIVE_API_PORT } from '@env';
+import axiosInstance from '../../utils/axiosInstance';
 
-const SERVER_URL = REACT_NATIVE_SOCKET_SERVER_URL;
+const SERVER_URL = `${REACT_NATIVE_API_URL}:${REACT_NATIVE_API_PORT}`;
 
 export default function PlayersList() {
-  const [players, setPlayers] = useState([
-    { user_id: 1, username: 'mathieu' },
-    { user_id: 2, username: 'julien' },
-    { user_id: 3, username: 'fred' },
-    { user_id: 4, username: 'sylvain' },
-  ]);
+  const [players, setPlayers] = useState([]);
+  const [socket, setSocket] = useState(null);
+
+  const [quizId, setQuizId] = useState(null);
+  const [gameId, setGameId] = useState(null);
+
+  const createQuiz = async () => {
+    try {
+      const request = await axiosInstance.post('/quiz', {
+        amount: 5,
+        category: 15,
+        difficulty: 'medium',
+        title: 'test',
+      });
+      const quizId = request.data.quiz_id;
+      setQuizId(quizId);
+      console.log('Quiz ID:', quizId);
+    } catch (error) {
+      console.error('Error creating the quiz:', error);
+    }
+  };
+
+  const initGame = async () => {
+    try {
+      const request = await axiosInstance.post(`/game/${quizId}/init`, {
+        mode: 'scrum',
+        max_players: 5,
+      });
+      const gameId = request.data.game_id;
+      setGameId(gameId);
+      console.log('Game ID:', gameId);
+    } catch (error) {
+      console.error('Error initializing the game:', error);
+    }
+  };
+
+  const connectToSocketServer = async () => {
+    try {
+      const userToken = await AsyncStorage.getItem('userToken');
+
+      if (socket) {
+        socket.disconnect();
+      }
+
+      const newSocket = io(SERVER_URL, {
+        transports: ['websocket'],
+        extraHeaders: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      });
+
+      newSocket.on('connect', () => {
+        console.log('Connecté au serveur WebSocket');
+      });
+
+      newSocket.on('playerJoined', (updatedPlayerList) => {
+        setPlayers(updatedPlayerList.playerList);
+        console.log('Updated player list:', updatedPlayerList.playerList);
+      });
+
+      newSocket.on('', (updatedPlayerList) => {
+        setPlayers(updatedPlayerList.playerList);
+        console.log('Updated player list:', updatedPlayerList.playerList);
+      });
+
+      newSocket.on('disconnect', () => {
+        console.log('Déconnecté du serveur WebSocket');
+      });
+
+      setSocket(newSocket);
+    } catch (error) {
+      console.error('Erreur lors de la connexion au WebSocket :', error);
+    }
+  };
+
+  const joinGame = async () => {
+    try {
+      setPlayers([]);
+      socket.emit('joinGame', {
+        game_id: gameId,
+      });
+      console.log('Joining the game with ID:', gameId);
+    } catch (error) {
+      console.error('Error joining the game:', error);
+    }
+  };
 
   useEffect(() => {
-    const userToken = AsyncStorage.getItem('userToken');
-
-    const socket = io(SERVER_URL, {
-      transports: ['websocket'],
-      extraHeaders: {
-        Authorization: `Bearer ${userToken}`,
-      },
-    });
-
-    socket.on('connect', () => {
-      console.log('Connecté au serveur WebSocket');
-    });
-
-    socket.on('playerJoined', (data) => {
-      setPlayers(data);
-      console.log('player joined the game:', data);
-    });
-
-    socket.on('disconnect', () => {
-      console.log('Déconnecté du serveur WebSocket');
-    });
-
     return () => {
-      socket.disconnect();
+      if (socket) {
+        socket.disconnect();
+      }
     };
-  }, []);
+  }, [socket]);
 
   return (
     <View style={styles.container}>
-      {players.map((player) => (
-        <Text style={styles.username} key={player.user_id}>
-          {player.username}
+      <Button title="Create Quiz" onPress={createQuiz} />
+      <Button title="Init Game" onPress={initGame} />
+      <Button title="Connect to WebSocket" onPress={connectToSocketServer} />
+      <Button title="Join Game" onPress={joinGame} />
+      {players.map((player, index) => (
+        <Text style={styles.username} key={index}>
+          {player}
         </Text>
       ))}
     </View>
