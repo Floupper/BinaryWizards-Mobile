0d117d1f407190767783c33874e8542bf6689562
Eliott Schott <eliott.schott.eliott@gmail.com>
2024-12-20 16:32:51 +0100

    feat: added mogula font

--- BinaryWizards-MobileApp/App.js
+++ BinaryWizards-MobileApp/App.js
@@ -1,11 +1,17 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import Toast from 'react-native-toast-message';
+import * as Font from 'expo-font';
 
 import StackNavigator from './src/navigation/StackNavigator';
 
 export default function App() {
+  const [fontsLoaded] = Font.useFonts({
+    Mogula: require('./assets/fonts/mogula.otf'),
+  });
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
