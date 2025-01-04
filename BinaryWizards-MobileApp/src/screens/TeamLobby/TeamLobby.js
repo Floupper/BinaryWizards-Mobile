d2053a817d9337d1df0244d17e6c2f02067d8c6b
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2025-01-04 10:39:06 +0100

    feat(TeamJoin & TeamLobby): add structure & logic to team game mode

---
+++ BinaryWizards-MobileApp/src/screens/TeamLobby/TeamLobby.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+
+import ShareModal from '../../components/ShareModal/ShareModal';
+import PlayersList from '../../components/PlayersList/PlayersList';
+
+export default function TeamLobby({ route }) {
+  const [gameId, setGameId] = useState(route.params.teamCode);
+  const [gameMode, setGameMode] = useState(route.params.gameMode);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  console.log(gameId, gameMode);
+
+  const toggleModal = () => {
+    setIsModalVisible(!isModalVisible);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Players List</Text>
+      <PlayersList game_id={gameId} game_mode={gameMode} />
+
+      <TouchableOpacity onPress={toggleModal} style={styles.shareButton}>
+        <Text style={styles.shareButtonText}>Show share link</Text>
+        <Feather name="eye" size={20} color="#fff" />
+      </TouchableOpacity>
+      <ShareModal
+        visible={isModalVisible}
+        toggleModal={toggleModal}
+        gameId={gameId}
+      />
+    </View>
+  );
+}
