3e1e8f4a560ea62b8833cf0b4b9088022bc0cdb1
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2025-01-10 10:00:52 +0100

    feat(TeamLobby): add socket to have the start of teams game

--- BinaryWizards-MobileApp/src/screens/TeamLobby/TeamLobby.js
+++ BinaryWizards-MobileApp/src/screens/TeamLobby/TeamLobby.js
@@ -1,18 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { io } from 'socket.io-client';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Feather from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import ShareModal from '../../components/ShareModal/ShareModal';
 import PlayersList from '../../components/PlayersList/PlayersList';
+import { REACT_NATIVE_API_URL, REACT_NATIVE_API_PORT } from '@env';
+
+const SERVER_URL = `${REACT_NATIVE_API_URL}:${REACT_NATIVE_API_PORT}`;
 
 export default function TeamLobby({ route }) {
   const [gameId, setGameId] = useState(route.params.teamCode);
   const [gameMode, setGameMode] = useState(route.params.gameMode);
   const [isModalVisible, setIsModalVisible] = useState(false);
 
+  const navigation = useNavigation();
+
   const toggleModal = () => {
     setIsModalVisible(!isModalVisible);
   };
+
+  useEffect(() => {
+    const startGame = async () => {
+      try {
+        const userToken = await AsyncStorage.getItem('userToken');
+
+        const newSocket = io(SERVER_URL, {
+          transports: ['websocket'],
+          extraHeaders: {
+            Authorization: `Bearer ${userToken}`,
+          },
+        });
+
+        newSocket.on('gameStarted', () => {
+          navigation.navigate('Questions', { gameId });
+        });
+
+        newSocket.on('disconnect', () => {
+          console.log('Disconnected from WebSocket server');
+        });
+
+        setSocket(newSocket);
+      } catch (error) {
+        console.error('Error during connecting to websocket :', error);
+      }
+    };
+
+    startGame();
+  }, [gameId]);
 
   return (
     <View style={styles.container}>
