0d04a9a541a1fa0304d40b749067fbb5943d3b13
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2025-01-11 15:42:43 +0100

    feat: update StackNavigator to enable deep linking

--- BinaryWizards-MobileApp/src/navigation/StackNavigator.js
+++ BinaryWizards-MobileApp/src/navigation/StackNavigator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
 import QuestionScreen from '../screens/Questions/QuestionsScreen';
@@ -10,74 +11,129 @@ import TeamJoin from '../screens/TeamJoin/TeamJoin';
 import TeamLobby from '../screens/TeamLobby/TeamLobby';
 import ScrumLobby from '../screens/ScrumLobby/ScrumLobby';
 import ScrumJoin from '../screens/ScrumJoin/ScrumJoin';
+import { NavigationContainer } from '@react-navigation/native';
+import * as Linking from 'expo-linking';
+import SetGameMode from '../components/SetGameMode/SetGameMode';
+
+const prefix = Linking.createURL('/');
 
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
+  const [data, setData] = useState(null);
+
+  const linking = {
+    prefixes: [prefix],
+    config: {
+      screens: {
+        Home: 'home',
+        Create: 'create',
+        Questions: 'questions',
+        Questions: 'questions/:gameId/',
+        End: 'end',
+        ScrumJoin: 'scrumjoin',
+        TeamJoin: 'teamjoin',
+        Signup: 'signup',
+        Signin: 'signin',
+        SetGameMode: 'game/join/:gameId/',
+      },
+    },
+  };
+
+  function handleDeepLink(event) {
+    let data = Linking.parse(event.url);
+    setData(data);
+  }
+
+  useEffect(() => {
+    async function getInitialURL() {
+      const initialURL = await Linking.getInitialURL();
+      if (initialURL) {
+        let data = Linking.parse(initialURL);
+        setData(data);
+      }
+    }
+    Linking.addEventListener('url', handleDeepLink);
+    if (!data) {
+      getInitialURL();
+    }
+    return () => {
+      Linking.removeEventListener('url');
+    };
+  }, []);
+
   return (
     <>
-      {/* Define the stack navigator */}
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false, title: 'Home' }}
-        />
+      <NavigationContainer linking={linking}>
+        {/* Define the stack navigator */}
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false, title: 'Home' }}
+          />
 
-        <Stack.Screen
-          name="Create"
-          component={CreateGame}
-          options={{ headerShown: true, title: 'Create a game' }}
-        />
+          <Stack.Screen
+            name="Create"
+            component={CreateGame}
+            options={{ headerShown: true, title: 'Create a game' }}
+          />
 
-        <Stack.Screen
-          name="Questions"
-          component={QuestionScreen}
-          options={{ headerShown: true }}
-        />
+          <Stack.Screen
+            name="Questions"
+            component={QuestionScreen}
+            options={{ headerShown: true }}
+          />
 
-        <Stack.Screen
-          name="End"
-          component={EndScreen}
-          options={{ headerShown: true }}
-        />
+          <Stack.Screen
+            name="SetGameMode"
+            component={SetGameMode}
+            options={{ headerShown: false }}
+          />
 
-        <Stack.Screen
-          name="Signup"
-          component={Signup}
-          options={{ headerShown: true, title: 'Sign Up' }}
-        />
+          <Stack.Screen
+            name="End"
+            component={EndScreen}
+            options={{ headerShown: true }}
+          />
 
-        <Stack.Screen
-          name="Signin"
-          component={Signin}
-          options={{ headerShown: true, title: 'Sign In' }}
-        />
+          <Stack.Screen
+            name="Signup"
+            component={Signup}
+            options={{ headerShown: true, title: 'Sign Up' }}
+          />
 
-        <Stack.Screen
-          name="ScrumLobby"
-          component={ScrumLobby}
-          options={{ headerShown: true, title: 'Scrum Lobby' }}
-        />
+          <Stack.Screen
+            name="Signin"
+            component={Signin}
+            options={{ headerShown: true, title: 'Sign In' }}
+          />
 
-        <Stack.Screen
-          name="ScrumJoin"
-          component={ScrumJoin}
-          options={{ headerShown: true, title: 'Scrum Join' }}
-        />
+          <Stack.Screen
+            name="ScrumLobby"
+            component={ScrumLobby}
+            options={{ headerShown: false, title: 'Scrum Lobby' }}
+          />
 
-        <Stack.Screen
-          name="TeamJoin"
-          component={TeamJoin}
-          options={{ headerShown: true, title: 'Team Join' }}
-        />
+          <Stack.Screen
+            name="ScrumJoin"
+            component={ScrumJoin}
+            options={{ headerShown: true, title: 'Scrum Join' }}
+          />
 
-        <Stack.Screen
-          name="TeamLobby"
-          component={TeamLobby}
-          options={{ headerShown: true, title: 'Team Lobby' }}
-        />
-      </Stack.Navigator>
+          <Stack.Screen
+            name="TeamJoin"
+            component={TeamJoin}
+            options={{ headerShown: true, title: 'Team Join' }}
+          />
+
+          <Stack.Screen
+            name="TeamLobby"
+            component={TeamLobby}
+            options={{ headerShown: false, title: 'Team Lobby' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
